cc708e080b535cf0c12bab8c121ac2ffad6d8c82
dodo-sudo <arekscargo@gmail.com>
2024-11-11 02:19:20 +0300

    Update verify.js

--- netlify-functions/verify.js
+++ netlify-functions/verify.js
@@ -1,14 +1,14 @@
 const tcKimlikDogrulama = require('tc-kimlik-dogrulama');
 
 exports.handler = async function(event, context) {
-    // Body verilerini JSON olarak alıyoruz
+  
     const { ad, soyad, dogumYili, tcKimlikNo } = JSON.parse(event.body);
 
     try {
-        // TC Kimlik doğrulama işlemini yapıyoruz
+      
         const result = await tcKimlikDogrulama({ ad, soyad, dogumYili, tcKimlikNo });
 
-        // Başarılıysa true, başarısızsa false döndürüyoruz
+      
         return {
             statusCode: 200,
             body: JSON.stringify(result)
